efd0994f2f3b2b61191db5107d31e8db90314b0e
PamelaOrtiz-01 <ortizpamela2001@gmail.com>
2026-09-09 17:50:13 -0700

    Metodo check

--- Hang_man/hang_man_functions.py
+++ Hang_man/hang_man_functions.py
@@ -14,17 +14,31 @@ class Hangman:
         self.list_of_guesses = []
     def load_board(self):
         """Load the the game board."""
-        for i in range(0,8):
+        for i in range(0,self.LIVES+1):
             with open(f"board_{i}.txt", "r") as f:
                 self.board[i] = f.read()
     def display_board(self):
         """Display the current state of the game board."""
-        print(self.board[self.num_lives])
+        print(self.board[self.LIVES - self.num_lives])
         print(" ".join(self.word_guessed))
         print(f"Lives remaining: {self.num_lives}")
         print(f"Guessed letters: {', '.join(self.list_of_guesses)}")
+    def check_guess(self, guess:str):
+        """Check if the guessed letter is in the word."""
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+                self.num_letters -= 1
+            else:
+                print(f"Sorry, {guess} is not in the word.")
+                self.num_lives -= 1
+            self.list_of_guesses.append(guess)                
 if __name__ == "__main__":
-    word_list = ["python", "java", "javascript", "hangman", "programming"]
-    hangman_game = Hangman(word_list)
+    list_word = ["python", "java", "javascript", "hangman", "programming"]
+    hangman_game = Hangman(list_word)
     hangman_game.load_board()
+    hangman_game.check_guess("p")
     hangman_game.display_board()
