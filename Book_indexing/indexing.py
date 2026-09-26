61e5c553c19ac36bd426fd351cc3f4dff1e3ad1f
PamelaOrtiz-01 <ortizpamela2001@gmail.com>
2026-09-25 17:45:11 -0700

    Updated main method to print Frequencies

--- Book_indexing/indexing.py
+++ Book_indexing/indexing.py
@@ -7,7 +7,7 @@ import itertools
 from auxiliary_functions import list_directory_files, load_book, clean_list_of_words, reduce_list_of_words, count_words   
 
 def compute_tf(word_count, total_words):
-    """"""
+    """Compute the term frequency (TF) for a word in a document."""
     if total_words == 0:
         return{}
     dict_tf = {}
@@ -33,6 +33,18 @@ def main(args):
     else:
         print(f"Error: The path '{book_path}' is neither a file nor a directory.")
     print(book_dictionary.keys())
+    for book_name, words in book_dictionary.items():
+        cleaned_words = clean_list_of_words(words)
+        word_count = count_words(cleaned_words)
+        total_words = len(cleaned_words)
+        tf_dict = compute_tf(word_count, total_words)
+        print(f"Book: {book_name}")
+        print(f"Total words: {total_words}")
+        print(f"Unique words: {len(word_count)}")
+        print(f"Term Frequencies:")
+        for word, tf in tf_dict.items():
+            if tf >=0.0001: # Only print words with a non-zero frequency
+                print(f" {word}: {tf:.4f}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Index books.")
